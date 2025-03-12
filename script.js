e663ce903c079f9b4d6a4e53d6585a7a5ddc1001
Thiego Ribeiro <thiego.hl@gmail.com>
2025-03-11 22:35:30 -0300

    obtendo dados do formulário

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const USD = 5.87
 const EUR = 6.12
 const GBP = 7.30
+
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
